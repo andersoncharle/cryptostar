050d626a9709b6ff6266f5c166928a4c30e4a805
blackmitnick <mkakarianderson68@gmail.com>
2022-04-23 18:01:31 +0300

    user profile admin badges changes

--- src/App.vue
+++ src/App.vue
@@ -20,14 +20,14 @@ body {
 }
 #app {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-  padding: 50px;
-  width: 50%;
+  /*padding: 50px;*/
+  /*width: 50%;*/
   background-color: MediumSeaGreen;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  /*color: ghostwhite;*/
-  margin: 0 auto 60px;
+  /*text-align: center;*/
+  /*!*color: ghostwhite;*!*/
+  /*margin: 0 auto 60px;*/
 }
 </style>
