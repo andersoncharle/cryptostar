25a4fb7336d87cfe6cb87ac46b9a8184a8e453e3
blackmitnick <mkakarianderson68@gmail.com>
2022-04-23 18:05:21 +0300

    user profile changes with main app

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ body {
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   /*padding: 50px;*/
   /*width: 50%;*/
-  background-color: MediumSeaGreen;
+  /*background-color: MediumSeaGreen;*/
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
